fe4179e6f810139a0ddbfd550e732dd77da2b0a5
Francis Grégoire <francis.gregoire.1@etsmtl.net>
2018-12-05 23:50:01 -0500

    cleanup

--- Day 5/day5.part2.ps1
+++ Day 5/day5.part2.ps1
@@ -1,54 +1,56 @@
 $start = Get-Date
 $inputday = [System.Collections.ArrayList](Get-Content .\day5.input).ToCharArray()
 
-$uniques = $inputday | select -Unique
-#write-output $inputday
+$uniques = $inputday | sort-object -Unique
 
 
-function FunctionName {
-    param (
-        [System.Collections.ArrayList]$potato,
-        [char]$letter
-    )
-    $modified = $true
-    while ($modified) {
-        $modified = $false
-        for ($i = 0; $i -lt $potato.Count; $i++) {
-            if ($potato[$i] -like $letter) {
-                $potato.RemoveAt($i)
-                $modified = $true
-                break
-            }
-            if (($potato[$i] -like $potato[$i + 1]) -and (-not($potato[$i] -clike $potato[$i + 1]))) {
-                $potato.RemoveAt($i + 1)
-                $potato.RemoveAt($i)
-                $modified = $true
-                break
-            }
-        }
-    } 
+function ShrinkArray {
+  param (
+    [System.Collections.ArrayList]$potato,
+    [char]$letter
+  )
+  $modified = $true
+  while ($modified) {
+    $modified = $false
+    #Write-Output "Current length : $($potato.Count)"
 
-    return $potato.Count
-}
-$min = 1000000000
-workflow poss {
-    param($arry)
-    foreach -parallel ($puke in $arry) {
-        FunctionName -potato $inputday.Clone() -letter $puke
+    for ($i = 0; $i -lt $potato.Count; $i++) {
+      Write-Progress -id 2 -ParentId 1 -Activity "Processing data" -PercentComplete ($i / $inputday.Count * 100) -Status "Current length : $($potato.Count)"
+      if ($i -lt 0) {
+        $i = 0
+      }
+      if ($potato[$i] -like $letter) {
+        $potato.RemoveAt($i)
+        $modified = $true
+        $i -= 2
+      }
+      if (($potato[$i] -like $potato[$i + 1]) -and (-not($potato[$i] -clike $potato[$i + 1]))) {
+        $potato.RemoveAt($i + 1)
+        $potato.RemoveAt($i)
+        $modified = $true
+        $i -= 2
+
+      }
     }
+  }
+  return $potato.Count
 }
 
-<#
-$i = 0
+
+$j = 0
+$min = 50000
 foreach ($u in $uniques) {
-    Write-Progress -Activity "Calulating length with $u ||| Current min is : $min" -Status "Letter : $u ||| Min : $min" -percentComplete ($i / $uniques.Count * 100)
-    $temp = FunctionName -potato $inputday.Clone() -letter $u
-    if ($temp -lt $min) {
-        $min = $temp
-    }
+  Write-Progress -Activity "Calulating length with $u || Current min is : $min || Time since started $((Get-date) - $start)" -Status "Letter : $u ||| Min : $min" -percentComplete ($j / $uniques.Count * 100) -Id (1)
+  $temp = ShrinkArray -potato $inputday.Clone() -letter $u
+  if ($temp -lt $min) {
+    $min = $temp
+  }
+  $j++
 }
-    
-#>
+
+
+
+
 
 $end = Get-Date
 
