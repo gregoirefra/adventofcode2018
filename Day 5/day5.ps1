c962044eb8479442f3171cf526ba11c18082b4ca
Francis Grégoire <francis.gregoire.1@etsmtl.net>
2018-12-05 23:20:56 -0500

    forgot to commit this and clean

--- Day 5/day5.ps1
+++ Day 5/day5.ps1
@@ -3,13 +3,20 @@ $inputday = [System.Collections.ArrayList](Get-Content .\day5.input).ToCharArray
 #write-output $inputday
 $modified = $true
 while ($modified) {
-    $modified=$false
-    for ($i = 0; $i -lt $inputday.Count; $i++) {
-        if (($inputday[$i] -like $inputday[$i + 1]) -and (-not($inputday[$i] -clike $inputday[$i + 1]))) {
-            $inputday.RemoveAt($i + 1)
-            $inputday.RemoveAt($i)
-            $modified = $true
-        }
+  Write-Output "Current length : $($inputday.Count)"
+  $modified = $false
+  Write-Progress -id 1 -Activity "Day 5 part 1" -Status "Shrinking data"
+  for ($i = 0; $i -lt $inputday.Count; $i++) {
+    if ($i -lt 0) {
+      $i = 0
     }
-    return $inputday
-} 
+    Write-Progress -id 2 -ParentId 1 -Activity "Processing data" -PercentComplete ($i / $inputday.Count * 100) 
+    if (($inputday[$i] -like $inputday[$i + 1]) -and (-not($inputday[$i] -clike $inputday[$i + 1]))) {
+      $inputday.RemoveAt($i + 1)
+      $inputday.RemoveAt($i)
+      $modified = $true
+      $i -= 2
+    }
+  }
+}
+write-output "Final length: $($inputday.Count)"
